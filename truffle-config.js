05f2e85f2ca60d07209b41c078285e484340430e
hyun2 <hks0284@gmail.com>
2022-02-27 11:36:26 +0900

    feat: init

---
+++ truffle-config.js
@@ -0,0 +1,79 @@
+const Web3 = require("web3");
+const { projectId, mnemonic, API_KEY } = require("./secrets.json");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+Web3.providers.HttpProvider.prototype.sendAsync =
+  Web3.providers.HttpProvider.prototype.send;
+
+const protocol = "http";
+const ip = "127.0.0.1";
+const port = 9650;
+const provider = new Web3.providers.HttpProvider(
+  `${protocol}://${ip}:${port}/ext/bc/C/rpc`
+);
+
+const privateKeys = [
+  "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027",
+  "0x7b4198529994b0dc604278c99d153cfd069d594753d471171a1d102a10438e07",
+  "0x15614556be13730e9e8d6eacc1603143e7b96987429df8726384c2ec4502ef6e",
+  "0x31b571bf6894a248831ff937bb49f7754509fe93bbd2517c9c73c4144c0e97dc",
+  "0x6934bef917e01692b789da754a0eae31a8536eb465e7bff752ea291dad88c675",
+  "0xe700bdbdbc279b808b1ec45f8c2370e4616d3a02c336e68d85d4668e08f53cff",
+  "0xbbc2865b76ba28016bc2255c7504d000e046ae01934b04c694592a6276988630",
+  "0xcdbfd34f687ced8c6968854f8a99ae47712c4f4183b78dcc4a903d1bfe8cbf60",
+  "0x86f78c5416151fe3546dece84fda4b4b1e36089f2dbc48496faf3a950f16157c",
+  "0x750839e9dbbd2a0910efe40f50b2f3b2f2f59f5580bb4b83bd8c1201cf9a010a",
+];
+
+module.exports = {
+  networks: {
+    development: {
+      provider: () => {
+        return new HDWalletProvider({
+          privateKeys: privateKeys,
+          providerOrUrl: provider,
+        });
+      },
+      network_id: "*",
+      gas: 3000000,
+      gasPrice: 225000000000,
+    },
+    dev: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${projectId}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.11", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
+};
